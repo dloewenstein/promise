4873dc1ec2be052ac8be21b9aa06a1329e4cfe2d
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2019-01-08 17:41:23 +0100

    reminder of adminrights 1st time use

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname){
-    packageStartupMessage("For first time use, run set_data_path")
+    packageStartupMessage("For first time use\nstart R with adminrights and then run set_data_path")
 }
