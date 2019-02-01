f5568c1fe3966074285e3eb97110793b34d90207
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2019-02-01 14:02:47 +0100

    DATADIGEST not in installed folder, remove check

--- R/zzz.R
+++ R/zzz.R
@@ -10,12 +10,5 @@
             packageStartupMessage(cat(crayon::underline(cli::symbol$info, "For first time use run `set_data_path()`")))
         }
         
-    } else {
-        load(file.path(.libPaths()[1], "promise", "data", "promise.rda"))
-        old_digest <- read.dcf(file.path(.libPaths()[1], "promise", "DATADIGEST"))
-        matching <- old_digest[, "promise"] == digest::digest(promise)
-        
-        message(cat(crayon::red(cli::symbol$warning, 
-                                "current promise data doesn't match package version\nRun `set_data_path()` and reference to", sprintf("promise_v.%s.rda", old_digest[, "DataVersion"]))))
     }
 }
