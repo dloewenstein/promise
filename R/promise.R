b0d4ab0588de70d9caa1526c4fd9f6af6d8fd9a7
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2019-04-01 11:23:23 +0200

    docs: fixed description promise package

--- R/promise.R
+++ R/promise.R
@@ -1,11 +1,11 @@
-#' promise
-#' A data package for promise.
+#' promise-package
+#'
+#' A data package for the PROMISE database.
+#'
 #' @docType package
 #' @aliases promise-package
-#' @title Package Title
-#' @name promise
-#' @description A description of the data package
-#' @details Use \code{data(package='promise')$results[, 3]} tosee a list of availabledata sets in this data package
+#' @name promise-package
+#' @details Use \code{data(package='promise')$results[, 3]} to see a list of available data sets in this data package
 #'     and/or DataPackageR::load_all
 #' _datasets() to load them.
 #' @seealso
